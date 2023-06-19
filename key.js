2459f5d7c765e107b982b554145cbbf6bc411cbf
denolk <revolter00@gmail.com>
2023-06-20 00:17:06 +0300

    mcdu function keys

--- key.js
+++ key.js
@@ -1,59 +1,47 @@
-// inject this script into the McduButton to get access to the socket
-// webpack://fbw-simbridge/apps/mcdu/src/components/McduButtons.jsx
-
-// window._mcduSoc = socket;
-
-// ws://10.0.0.35:8380/interfaces/v1/mcdu
-
-const _mcduOpts = {
-  rowInput: false,
-  rowInputToggleKey: 'Enter',
-};
-
-const _mcduNavKeys = {
+const _mcduFunctionKeys = {
+  Tab: 'DIR',
+  Insert: 'PROG',
+  Home: 'PERF',
+  PageUp: 'INIT',
+  Enter: 'DATA',
+  Delete: 'FPLN',
+  End: 'RAD',
+  PageDown: 'FUEL',
+  Escape: 'MENU',
+  Shift: 'AIRPORT',
   ArrowLeft: 'PREVPAGE',
   ArrowRight: 'NEXTPAGE',
   ArrowUp: 'UP',
   ArrowDown: 'DOWN',
   Backspace: 'CLR',
+  Space: 'SP',
   '.': 'DOT',
-  '/': 'DIV',
   '+': 'PLUSMINUS',
   '-': 'PLUSMINUS',
-};
-
-const _mcduRowInputToggle = (isOn) => {
-  _mcduOpts.rowInput = isOn;
-  document.querySelectorAll('div.button-grid:first-child div.button').forEach(n => { n.style.border = _mcduOpts.rowInput ? 'solid 1px rgba(0,255,0,0.4)' : 'none' });
+  '/': 'DIV',
+  '*': 'OVFY',
 };
 
 const _mcduGetKey = (keyEvent) => {
-  // match mcdu L/R row input F keys with Enter toggle
-  if (_mcduOpts.rowInput && keyEvent.key.match(/F\d+/)) {
+  // match a single char
+  if (keyEvent.key.length === 1 && keyEvent.key.match(/[\w\d]/i)) {
+    return keyEvent.key.toLocaleUpperCase();
+  }
+
+  // match mcdu L/R row input for F keys
+  if (keyEvent.key.match(/F\d+/)) {
     const fn = parseInt(keyEvent.key.replace('F', ''));
     return fn <= 6 ? `L${fn}` : `R${fn - 6}`;
   }
-  // match a single char
-  else if (keyEvent.key.length === 1 && keyEvent.key.match(/[\w\d]/i)) {
-    return keyEvent.key.toLocaleUpperCase();
-  }
-  // match mcdu nav keys
+  // match mcdu function keys
   else {
-    return _mcduNavKeys[keyEvent.key];
+    return _mcduFunctionKeys[keyEvent.key];
   }
 };
 
 let _mcduKeyboardInput = (keyEvent) => {
   console.log('event', keyEvent);
-
-  if (keyEvent.key === _mcduOpts.rowInputToggleKey) {
-    _mcduRowInputToggle(!_mcduOpts.rowInput);
-    return;
-  }
-
-  if (keyEvent.key.match(/F\d+/)) {
-    keyEvent.preventDefault();
-  }
+  keyEvent.preventDefault();
 
   const key = _mcduGetKey(keyEvent);
 
@@ -62,11 +50,10 @@ let _mcduKeyboardInput = (keyEvent) => {
   }
 
   console.log(`mcdu key: ${key}`);
-  window._mcduSoc?.send(`event:left:${key}`);
 
-  _mcduRowInputToggle(false);
+  window._mcduSoc?.send(`event:left:${key}`);
 };
 
-window._mcduSoc = new WebSocket(`ws://${window.location.host}/interfaces/v1/mcdu`);
 window.onbeforeunload = () => { return false; }
+window._mcduSoc = new WebSocket(`ws://${window.location.host}/interfaces/v1/mcdu`);
 window.addEventListener('keydown', _mcduKeyboardInput);
---
+++ readme.md
@@ -0,0 +1,40 @@
+## keyboard inputs for flybywire remote mcdu display
+
+provides keyboard inputs for flybywire remote mcdu display on browser.
+
+1. open remote mcdu display on browser (http://localhost:8380/interfaces/mcdu)
+2. inject `key.js` into the page
+3. good to go
+
+### keyboard - mcdu key mapping
+
+| keyboard    | mcdu key  |
+| ----------- | --------- |
+| `Tab`       | DIR       |
+| `Insert`    | PROG      |
+| `Home`      | PERF      |
+| `PageUp`    | INIT      |
+| `Enter`     | DATA      |
+| `Delete`    | FPLN      |
+| `End`       | RAD NAV   |
+| `PageDown`  | FUEL PRED |
+| `Esc`       | MENU      |
+| `Shift`     | AIRPORT   |
+| `F1`-`F6`   | LSK1-LSK6 |
+| `F7`-`F12`  | RSK1-RSK6 |
+| `Left`      | PREVPAGE  |
+| `Right`     | NEXTPAGE  |
+| `Up`        | UP        |
+| `Down`      | DOWN      |
+| `Backspace` | CLR       |
+| `Space`     | SP        |
+| `.`         | DOT       |
+| `+` `-`     | PLUSMINUS |
+| `/`         | DIV       |
+| `*`         | OVFY      |
+| `A-Z` `0-9` | A-Z 0-9   |
+
+### refs
+
+- [fbw simbridge mcdu display](https://docs.flybywiresim.com/simbridge/simbridge-feature-guides/remote-displays/remote-mcdu/)
+- [fbw mcdu interface doc](https://docs.flybywiresim.com/pilots-corner/a32nx-briefing/mcdu/interface/)
